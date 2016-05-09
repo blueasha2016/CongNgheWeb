714e7ac75d6d77ad7ab0feff37854d1b98a2879e
blueasha2016 <kyo.solo.kute@gmail.com>
2016-05-09 11:05:33 +0700

    chage database

--- database dump/pizza_inn.sql
+++ database dump/pizza_inn.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.1
+-- version 4.1.12
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: May 07, 2013 at 03:10 PM
--- Server version: 5.5.24-log
--- PHP Version: 5.4.3
+-- Host: 127.0.0.1
+-- Generation Time: May 09, 2016 at 07:04 AM
+-- Server version: 5.6.16
+-- PHP Version: 5.5.11
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -29,11 +29,11 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `billing_details` (
   `billing_id` int(10) NOT NULL AUTO_INCREMENT,
   `member_id` int(15) NOT NULL,
-  `Street_Address` varchar(100) NOT NULL,
-  `P_O_Box_No` varchar(15) NOT NULL,
+  `Street_Address` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `P_O_Box_No` varchar(15) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `City` text NOT NULL,
-  `Mobile_No` varchar(15) NOT NULL,
-  `Landline_No` varchar(15) DEFAULT NULL,
+  `Mobile_No` varchar(15) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `Landline_No` varchar(15) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`billing_id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
@@ -51,7 +51,7 @@ CREATE TABLE IF NOT EXISTS `cart_details` (
   `total` float NOT NULL,
   `flag` int(1) NOT NULL,
   PRIMARY KEY (`cart_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -61,9 +61,9 @@ CREATE TABLE IF NOT EXISTS `cart_details` (
 
 CREATE TABLE IF NOT EXISTS `categories` (
   `category_id` int(15) NOT NULL AUTO_INCREMENT,
-  `category_name` varchar(45) NOT NULL,
+  `category_name` varchar(45) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`category_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -73,10 +73,10 @@ CREATE TABLE IF NOT EXISTS `categories` (
 
 CREATE TABLE IF NOT EXISTS `currencies` (
   `currency_id` int(5) NOT NULL AUTO_INCREMENT,
-  `currency_symbol` varchar(15) NOT NULL,
+  `currency_symbol` varchar(15) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `flag` int(1) NOT NULL,
   PRIMARY KEY (`currency_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -86,13 +86,13 @@ CREATE TABLE IF NOT EXISTS `currencies` (
 
 CREATE TABLE IF NOT EXISTS `food_details` (
   `food_id` int(15) NOT NULL AUTO_INCREMENT,
-  `food_name` varchar(45) NOT NULL,
+  `food_name` varchar(45) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `food_description` text NOT NULL,
   `food_price` float NOT NULL,
-  `food_photo` varchar(45) NOT NULL,
+  `food_photo` varchar(45) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `food_category` int(15) NOT NULL,
   PRIMARY KEY (`food_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=24 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -102,14 +102,21 @@ CREATE TABLE IF NOT EXISTS `food_details` (
 
 CREATE TABLE IF NOT EXISTS `members` (
   `member_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `firstname` varchar(100) DEFAULT NULL,
-  `lastname` varchar(100) DEFAULT NULL,
-  `login` varchar(100) NOT NULL DEFAULT '',
-  `passwd` varchar(32) NOT NULL DEFAULT '',
+  `firstname` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `lastname` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `login` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `passwd` varchar(32) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   `question_id` int(5) NOT NULL,
   `answer` varchar(45) NOT NULL,
   PRIMARY KEY (`member_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
+
+--
+-- Dumping data for table `members`
+--
+
+INSERT INTO `members` (`member_id`, `firstname`, `lastname`, `login`, `passwd`, `question_id`, `answer`) VALUES
+(15, 'Nguyá»…n HoÃ ng', 'QuÃ¢n', 'hoangquan@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 9, 'e377fbada15a330f9f55901b3f81e0d0');
 
 -- --------------------------------------------------------
 
@@ -119,7 +126,7 @@ CREATE TABLE IF NOT EXISTS `members` (
 
 CREATE TABLE IF NOT EXISTS `messages` (
   `message_id` int(15) NOT NULL AUTO_INCREMENT,
-  `message_from` varchar(25) NOT NULL,
+  `message_from` varchar(25) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `message_date` date NOT NULL,
   `message_time` time NOT NULL,
   `message_subject` text NOT NULL,
@@ -153,9 +160,9 @@ CREATE TABLE IF NOT EXISTS `orders_details` (
 
 CREATE TABLE IF NOT EXISTS `partyhalls` (
   `partyhall_id` int(5) NOT NULL AUTO_INCREMENT,
-  `partyhall_name` varchar(45) NOT NULL,
+  `partyhall_name` varchar(45) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`partyhall_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -168,7 +175,14 @@ CREATE TABLE IF NOT EXISTS `pizza_admin` (
   `Username` varchar(45) NOT NULL,
   `Password` varchar(45) NOT NULL,
   PRIMARY KEY (`Admin_ID`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `pizza_admin`
+--
+
+INSERT INTO `pizza_admin` (`Admin_ID`, `Username`, `Password`) VALUES
+(3, 'admin', 'admin');
 
 -- --------------------------------------------------------
 
@@ -194,7 +208,7 @@ CREATE TABLE IF NOT EXISTS `quantities` (
   `quantity_id` int(5) NOT NULL AUTO_INCREMENT,
   `quantity_value` int(5) NOT NULL,
   PRIMARY KEY (`quantity_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=33 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -204,9 +218,21 @@ CREATE TABLE IF NOT EXISTS `quantities` (
 
 CREATE TABLE IF NOT EXISTS `questions` (
   `question_id` int(5) NOT NULL AUTO_INCREMENT,
-  `question_text` text NOT NULL,
+  `question_text` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`question_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
+
+--
+-- Dumping data for table `questions`
+--
+
+INSERT INTO `questions` (`question_id`, `question_text`) VALUES
+(1, 'Ngày sinh của bạn là gì?'),
+(9, 'Bạn thích chó hay mèo?'),
+(10, 'Người yêu của bạn tên gì?'),
+(11, 'Theo bạn con gà có mấy chân?'),
+(12, 'Năm này bạn bao nhiêu tuổi?'),
+(13, 'Bạn học lại mấy môn ở đại học?');
 
 -- --------------------------------------------------------
 
@@ -216,9 +242,9 @@ CREATE TABLE IF NOT EXISTS `questions` (
 
 CREATE TABLE IF NOT EXISTS `ratings` (
   `rate_id` int(5) NOT NULL AUTO_INCREMENT,
-  `rate_name` varchar(15) NOT NULL,
+  `rate_name` varchar(15) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`rate_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -248,12 +274,12 @@ CREATE TABLE IF NOT EXISTS `reservations_details` (
 
 CREATE TABLE IF NOT EXISTS `specials` (
   `special_id` int(15) NOT NULL AUTO_INCREMENT,
-  `special_name` varchar(25) NOT NULL,
-  `special_description` text NOT NULL,
+  `special_name` varchar(25) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `special_description` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `special_price` float NOT NULL,
   `special_start_date` date NOT NULL,
   `special_end_date` date NOT NULL,
-  `special_photo` varchar(45) NOT NULL,
+  `special_photo` varchar(45) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`special_id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
@@ -265,10 +291,10 @@ CREATE TABLE IF NOT EXISTS `specials` (
 
 CREATE TABLE IF NOT EXISTS `staff` (
   `StaffID` int(15) NOT NULL AUTO_INCREMENT,
-  `firstname` varchar(25) NOT NULL,
-  `lastname` varchar(25) NOT NULL,
-  `Street_Address` text NOT NULL,
-  `Mobile_Tel` varchar(20) NOT NULL,
+  `firstname` varchar(25) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `lastname` varchar(25) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `Street_Address` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `Mobile_Tel` varchar(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`StaffID`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
@@ -280,9 +306,9 @@ CREATE TABLE IF NOT EXISTS `staff` (
 
 CREATE TABLE IF NOT EXISTS `tables` (
   `table_id` int(5) NOT NULL AUTO_INCREMENT,
-  `table_name` varchar(45) NOT NULL,
+  `table_name` varchar(45) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`table_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -292,10 +318,10 @@ CREATE TABLE IF NOT EXISTS `tables` (
 
 CREATE TABLE IF NOT EXISTS `timezones` (
   `timezone_id` int(5) NOT NULL AUTO_INCREMENT,
-  `timezone_reference` varchar(45) NOT NULL,
+  `timezone_reference` varchar(45) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `flag` int(1) NOT NULL,
   PRIMARY KEY (`timezone_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -305,9 +331,9 @@ CREATE TABLE IF NOT EXISTS `timezones` (
 
 CREATE TABLE IF NOT EXISTS `users` (
   `id` smallint(6) NOT NULL AUTO_INCREMENT,
-  `username` varchar(30) NOT NULL,
-  `password` varchar(32) NOT NULL,
-  `email` varchar(100) NOT NULL,
+  `username` varchar(30) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(32) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
